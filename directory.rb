91a429bdd12de4a818e220482e37fc8c82ab6382
Rhys Kyte <rskyte@gmail.com>
2017-11-07 17:50:47 +0000

    added student input method for morea interactivity

--- directory.rb
+++ directory.rb
@@ -1,18 +1,17 @@
 #put students in array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Micharl Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
-
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+    
 def print_header
   puts "The students of Villains Academy"
   puts "----------------"
@@ -28,6 +27,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students!"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
