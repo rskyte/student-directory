d0ed0d2b1342f18726237604f83342e93b2f8371
Rhys Kyte <rskyte@gmail.com>
2017-11-07 18:20:28 +0000

    numbered entries, mod mode available to selectively print lists

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+$mod_mode = 0
 #put students in array
 def input_students 
   puts "Please enter the names of the students"
@@ -11,15 +12,34 @@ def input_students
   end
   students
 end
+
+def mod(string)
+  if $mod_mode == 1
+    if string[0].downcase == 'a'
+      true
+    else
+      false
+    end
+  elsif $mod_mode == 2
+    if string.length < 13
+      true
+    else
+      false
+    end
+  else
+    true
+  end
+end
     
+      
 def print_header
   puts "The students of Villains Academy"
   puts "----------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if (mod(student[:name]) == true)
   end
 end
 
@@ -31,3 +51,4 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
