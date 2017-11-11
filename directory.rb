0e205667c21e22ea0ce88c31f42f168ebab9e55f
Rhys Kyte <rskyte@gmail.com>
2017-11-11 02:11:30 +0000

    added prompt to input cohort and ability to sort by cohort

--- directory.rb
+++ directory.rb
@@ -1,27 +1,35 @@
 $mod_mode = 0
 
 def input_students 
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and their cohorts"
+  puts "Use this format 'Namey McName/cohort"
   puts "To finish just hit return twice"
   students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november, hobbies: 'things', birth_country: 'place', height: 0}
-    puts students.count == 1 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
-    name = gets.chomp
+  inp = gets.delete "\n"
+  input = inp.split("/")
+  while !input.empty? do
+    students << {name: input[0], cohort: (input[1] ? input[1].downcase.to_sym : :noCohortData), hobbies: 'things', birth_country: 'place', height: 0}
+    puts students.count == 1 ? "Now we have 1 student" : "Now we have #{students.count} students"
+    input = gets.chomp.split("/")
   end
   students
 end
 
-def mod(string)
-  if $mod_mode == 1
-    if string[0].downcase == 'a'
+def mod(student)
+  if $mod_mode == 1 #print all students with name beginning with a certain letter
+    if student[:name][0].downcase == 'a'
       true
     else
       false
     end
-  elsif $mod_mode == 2
-    if string.length < 13
+  elsif $mod_mode == 2 #print all students with a name less than a certain length
+    if student[:name].length < 13
+      true
+    else
+      false
+    end
+  elsif $mod_mode == 3 #print by cohort
+    if student[:cohort] == :november
       true
     else
       false
@@ -31,7 +39,6 @@ def mod(string)
   end
 end
     
-      
 def print_header
   puts "The students of Villains Academy"
   puts "----------------"
@@ -39,7 +46,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40) if (mod(student[:name]) == true)
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(40) if (mod(student) == true)
   end
 end
 
